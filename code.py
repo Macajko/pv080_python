b75082850b1b1a54107c557508d5cdb9d74bfd5e
Macajko <44619429+Macajko@users.noreply.github.com>
2022-04-28 12:40:50 +0200

    Update code.py

--- code.py
+++ code.py
@@ -26,8 +26,8 @@ def fetch_website(urllib_version, url):
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
     http = urllib.PoolManager()
-    r = http.request('GET', url)
-    return r.data
+    req = http.request('GET', url)
+    return req.data
 
 
 
